add81f34ba10f60d368fff5f3d071a44a279ed28
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-29 09:56:36 +0300

    Commit 5

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className="App">
-      <motion.form initial={{x: 0, translateX: 0}} animate={{ x: 0, translateX: "50%" }}>
+      <motion.form initial={{x: 0, transform:{translateX: 0}}} animate={{ x: 0, transform:{translateX: 100} }}>
         <h1>Create An Account</h1>
         <label>
           Email:
